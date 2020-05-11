804a2a56aa31baee913cefc91debc3491dd6facf
Randal Sean Harrison <rharriso@nd.edu>
2020-05-11 15:14:54 -0400

    updated skin, added favicons, added integration for tracking the relation of student pool to participation

--- js/main.js
+++ js/main.js
@@ -6,7 +6,6 @@ $(document).ready(function() {
   // Write the site footer
   $("#site-footer").load("site-footer.html");
 
-
   // Declare and clear the array on load
   var studentsArray = [];
 
@@ -18,35 +17,57 @@ $(document).ready(function() {
       studentsArray.push(data[i]);
     }
 
-    // Toggle show and hide the office movie
-    $("#office").on("click", function(){
-      $("#hidey").slideToggle();
-    });
+    // Sort the array
+    studentsArray = studentsArray.sort();
 
-    // Choose a name at random on click
-    $("#roller").on("click", function() {
-
-      // If array not empty
-      if (studentsArray.length > 0) {
-
-        // Randomly select a student
-        var randomStudent = Math.floor(Math.random() * Math.floor(studentsArray.length));
-
-        // Write the student's name
-        $("#randomStudent").prepend("<li class='list-group-item p-3 text-center'>" + studentsArray[randomStudent] + "</li>");
-
-        $("#randomStudent li").removeClass("active");
-        $("#randomStudent li").removeClass("p-4").addClass("p-3");
-        $("#randomStudent li:first-of-type").addClass("active").addClass("p-4");
-
-        // Pop the chosen student off the array
-        studentsArray.splice(randomStudent, 1);
-      }
-
-    });
+    for (let i = 0; i < studentsArray.length; i++) {
+      $("#student-list").append("<button class='btn btn-aliceblue mx-2 my-2 pl-0 student'><span class='badge badge-seconday badge-pill'><i class='fad fa-fw fa-user mr-1'></i></span>" + studentsArray[i] + "</button>");
+    }
 
   });
 
+
+// Toggle show and hide the office movie
+// $("#instructions").on("click", function(){
+//   $("#hidey").slideToggle();
+// });
+
+// Choose a name at random on click
+$("#roller").on("click", function() {
+
+  if ( $("#sample-students").length ) {
+    $("#sample-students").remove();
+  }
+
+  // If array not empty
+  if (studentsArray.length > 0) {
+
+    // Randomly select a student
+    var randomStudent = Math.floor(Math.random() * Math.floor(studentsArray.length));
+
+    var rando = studentsArray[randomStudent];
+
+    $("#student-list button:contains('" + rando + "')").removeClass("btn-aliceblue").addClass("btn-light").addClass("half-opacity");
+
+    // Write the student's name
+    $("#randomStudent").prepend("<li class='list-group-item text-left p-4 text-center'> <i class='mr-4 fad fa-fw fa-user'></i>" + rando + "</li>");
+
+    $("#randomStudent li").removeClass("bg-aliceblue");
+    $("#randomStudent li").removeClass("p-4").addClass("p-3").addClass("text-muted");
+    $("#randomStudent li > i").removeClass("fa-megaphone").addClass("fa-user").addClass("text-muted");
+    $("#randomStudent li:first-of-type").addClass("bg-aliceblue").addClass("p-4").removeClass("text-muted");
+    $("#randomStudent li:first-of-type > i").removeClass("fa-user").addClass("fa-megaphone").removeClass("text-muted");
+
+    // Pop the chosen student off the array
+    studentsArray.splice(randomStudent, 1);
+  }
+
+});
+
+// Turn the students red
+$("#student-list button").on("click", function(){
+  $(this).addClass("bg-red");
+});
 
 
 });
