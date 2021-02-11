70c4062b740ca960461e446fe114bb66170dcfea
Randal Sean Harrison <rharriso@nd.edu>
2021-02-11 16:41:27 -0500

    added save functionality

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
   var studentsArray = [];
 
   // read in the array of names
-  $.getJSON("json/"+ currentCourse +".json", function(data) {
+  $.getJSON("json/" + currentCourse + ".json", function(data) {
 
     // Push the data into an array
     for (let i = 0; i < data.length; i++) {
@@ -61,7 +61,7 @@ $(document).ready(function() {
       // $("#randomStudent li:first-of-type").addClass("text-muted");
 
       // Prepend it to the array
-      $("#randomStudent").prepend("<li class='list-group-item text-left p-4 text-center btn-aliceblue'> <i class='mr-4 fad fa-fw fa-user'></i>" + $(this).text() + "</li>");
+      $("#randomStudent").prepend("<li class='list-group-item text-left p-3 text-center btn-aliceblue'> <i class='mr-4 fad fa-fw fa-user'></i>" + $(this).text() + "</li>");
 
       aliceblue();
 
@@ -99,7 +99,7 @@ $(document).ready(function() {
       $("#student-list button[data-studentname='" + rando + "']").removeClass("btn-aliceblue").addClass("btn-light").addClass("half-opacity");
 
       // Write the student's name
-      $("#randomStudent").prepend("<li class='list-group-item text-left p-4 text-center'> <i class='mr-4 fad fa-fw fa-user'></i>" + rando + "</li>");
+      $("#randomStudent").prepend("<li class='list-group-item text-left p-3 text-center'> <i class='mr-4 fad fa-fw fa-user'></i>" + rando + "</li>");
 
 
       // Pop the chosen student off the array
@@ -111,6 +111,68 @@ $(document).ready(function() {
 
   });
 
+  // Reset dialog -------------------------------------------------------------
+  $("#reset").on("click", function() {
+
+    // Open the dialog box onClick
+    $("#dialog-confirm").dialog("open");
+
+    // remove the close button on the modal
+    $(".ui-dialog-titlebar-close").remove();
+    $(".ui-icon-alert").remove();
+    $(".ui-dialog-buttonset button:first-of-type").addClass("btn btn-default");
+    $(".ui-dialog-buttonset button:last-of-type").addClass("btn btn-primary");
+  });
+
+  $("#dialog-confirm").dialog({
+    autoOpen: false,
+    resizable: false,
+    height: "auto",
+    width: 400,
+    modal: true,
+    buttons: {
+      "Cancel": function() {
+        $(this).dialog("close");
+      },
+      "Reset Form": function() {
+        // Turns the green checked circles to open empty circles
+        window.location.reload();
+      }
+    }
+  });
+
+  // Get the date for saving to a filename
+  var d = new Date();
+  var month = d.getMonth() + 1;
+  var day = d.getDate();
+  var hours = d.getHours();
+  var amPm = "am";
+  if (hours >= 12) {
+    hours = hours - 12;
+    amPm = "pm";
+  }
+  var minutes = d.getMinutes();
+
+  // Concatenate date and file name
+  var fullDate = d.getFullYear() + '-' +
+    (('' + month).length < 2 ? '0' : '') + month + '-' +
+    (('' + day).length < 2 ? '0' : '') + day + "-" + hours + minutes + amPm;
+  var fileToSave = fullDate + "-participation.txt";
+
+  // Save button
+  $("#save-button").on("click", function() {
+
+    var listy = $("#randomStudent li").map(function() {
+      return $(this).text();
+    }).get().join("\n");
+
+    // $("#randomStudent li").text()
+
+    var blob = new Blob([listy], {
+      type: "text/txt;charset=utf-8"
+    });
+    saveAs(blob, fileToSave);
+  });
 
 });
 // document.ready
